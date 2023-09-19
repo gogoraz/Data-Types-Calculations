edf357cf93eeff1ad3da3d9d0885661439f27997
George Blagoev <gblgoev.2create@gmmail.com>
2023-09-20 00:21:24 +0300

    Adding date in Deposit Calculator

--- Deposit Calculator/Program.cs
+++ Deposit Calculator/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Deposit_Calculator
+﻿using System;
+
+namespace Deposit_Calculator
 {
   internal class Program
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello, World!");
+      /*
+            amount = deposited amount + term of deposit * (deposited amount * annual interest rate) / 12
+            From the console read 3 lines:
+
+            1.Deposited amount – real number in the range[100.00... 10000.00]
+
+            2.Term of the deposit(in months) – an integer in the range[1... 12]
+
+            3.Annual interest rate – real number in the range[0.00... 100.00]
+      */
+
     }
   }
 }
